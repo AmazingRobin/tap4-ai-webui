2b0aff00882df726b22fdddc6f21db445f2f35df
AmazingRobin <robinli0302@gmail.com>
2025-01-07 15:21:35 +0800

    feat: update

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -86,7 +86,7 @@ export default function Footer() {
               title={t('contactUs')}
               type='email'
             >
-              {t('contactUs')} - 999
+              {t('contactUs')}
             </a>
           </div>
         </div>
